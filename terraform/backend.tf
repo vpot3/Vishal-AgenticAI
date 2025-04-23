1f1d380e8c4ec550d280986d14c8597f9a8db994
vpot3 <130909835+vpot3@users.noreply.github.com>
2025-04-23 17:53:49 +0530

    Delete terraform/backend.tf

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = "RGAgenticAI"
-    storage_account_name = "straccountagenticai"
-    container_name = "tfstate"
-    key = "terrform.tfstate"
-  }
-}
