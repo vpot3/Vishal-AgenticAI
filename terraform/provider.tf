5c98c7dc0126f170076d576b3f6d93a69b1ecca4
vpot3 <130909835+vpot3@users.noreply.github.com>
2025-04-24 10:37:28 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
   features {}
-  subscription_id = "da2d52da-2e04-4dd4-b37a-0fea54660754"
-  tenant_id = "1e0f395d-01c6-48ba-bd6f-5f97064102f0"
-  client_id = "86c58d6d-424d-44b8-8ec4-eed1a72b8370"
+  # subscription_id = "da2d52da-2e04-4dd4-b37a-0fea54660754"
+  # tenant_id = "1e0f395d-01c6-48ba-bd6f-5f97064102f0"
+  # client_id = "86c58d6d-424d-44b8-8ec4-eed1a72b8370"
 }
